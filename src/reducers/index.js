683984d9cc7c2030fd687cfa810e13930dcadeee
Kate-Shepel <kateborichevskaya@gmail.com>
2024-07-09 17:00:58 +0200

    refactor: filter with useSelector directly in the component and excluding filtering from reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,8 +3,7 @@ const initialState = {
     heroesLoadingStatus: 'idle',
     filters: [],
     filtersLoadingStatus: 'idle',
-    activeFilter: 'all',
-    filteredHeroes: []
+    activeFilter: 'all'
 }
 
 const reducer = (state = initialState, action) => {
@@ -18,10 +17,7 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 heroes: action.payload,
-                heroesLoadingStatus: 'idle',
-                filteredHeroes: state.activeFilter === 'all' ? 
-                                action.payload : 
-                                action.payload.filter(item => item.element === state.activeFilter),
+                heroesLoadingStatus: 'idle'
             }
         case 'HEROES_FETCHING_ERROR':
             return {
@@ -29,22 +25,14 @@ const reducer = (state = initialState, action) => {
                 heroesLoadingStatus: 'error'
             }
         case 'HERO_DELETED': 
-            const newHeroList = state.heroes.filter(item => item.id !== action.payload);
             return {
                 ...state,
-                heroes: newHeroList,
-                filteredHeroes: state.activeFilter === 'all' ? 
-                                newHeroList :
-                                newHeroList.filter(item => item.element === state.activeFilter)
+                heroes: state.heroes.filter(item => item.id !== action.payload)
             }
         case 'HERO_CREATED':
-        const updatedHeroList = [...state.heroes, action.payload]
         return {
             ...state,
-            heroes: updatedHeroList,
-            filteredHeroes: state.activeFilter === 'all' ? 
-                            updatedHeroList :
-                            updatedHeroList.filter(item => item.element === state.activeFilter)
+            heroes: [...state.heroes, action.payload]
         }
         case 'FILTERS_FETCHING':
             return {
@@ -63,13 +51,9 @@ const reducer = (state = initialState, action) => {
                 filtersLoadingStatus: 'error'
             }
         case 'ACTIVE_FILTER_CHANGED':
-            console.log(action.payload, 'ACTIVE_FILTER_CHANGED', state.activeFilter)
             return {
                 ...state,
-                activeFilter: action.payload,
-                filteredHeroes: action.payload === 'all' ? 
-                                state.heroes :
-                                state.heroes.filter(item => item.element === action.payload),
+                activeFilter: action.payload
             }
         default: return state
     }
