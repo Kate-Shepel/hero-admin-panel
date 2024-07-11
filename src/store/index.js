6745029a372c75a4b601ec870f058d5f75cd99dd
Kate-Shepel <kateborichevskaya@gmail.com>
2024-07-11 14:01:00 +0200

    feat: configure store with Redux Toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
-import { legacy_createStore as createStore, combineReducers, compose, applyMiddleware} from 'redux';
-import { thunk } from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
@@ -12,31 +11,18 @@ const stringMiddleware = (store) => (next) => (action) => {  //next means dispat
   return next(action)
 };
 
-// const enhancer = (createStore) => (...args) => {
-//   const store = createStore(...args);
+const store = configureStore({
+  reducer: {heroes, filters},
+  middleWare: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+  devTools: process.env.NODE_ENV !== 'production'
+})
 
-//   const oldDispatch = store.dispatch;
-//   store.dispatch = (action) => {
-//     if(typeof action === 'string') {
-//       return oldDispatch({
-//         type: action
-//       })
-//     }
-//     return oldDispatch(action)
-//   }
-//   return store;
-// }
-
-const store = createStore(
-                    combineReducers({heroes, filters}),// = {heroes: heroes, filters: filters}
-                    compose(
-                      applyMiddleware(thunk, stringMiddleware),
-                      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                    )
-                    // compose(
-                    //   enhancer,
-                    //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                    // )
-                  );
+// const store = createStore(
+//                     combineReducers({heroes, filters}),// = {heroes: heroes, filters: filters}
+//                     compose(
+//                       applyMiddleware(thunk, stringMiddleware),
+//                       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//                     )
+//                   );
 
 export default store;
