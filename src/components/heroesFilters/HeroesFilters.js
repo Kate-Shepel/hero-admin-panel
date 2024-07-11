4b3ba3310c936c114b3e7a77ad1b4c0563a7874a
Kate-Shepel <kateborichevskaya@gmail.com>
2024-07-11 17:34:23 +0200

    feat: create filtersSlice with createSlice

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -11,7 +11,8 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
 
-import { filtersFetching, filtersFetched, filtersFetchingError, activeFilterChanged } from '../../actions';
+import { activeFilterChanged } from './filtersSlice';
+import { fetchFilters } from '../../actions';
 import Spinner from '../spinner/Spinner';
 
 const HeroesFilters = () => {
@@ -21,10 +22,7 @@ const HeroesFilters = () => {
     const {request} = useHttp();
 
     useEffect(() => {
-        dispatch(filtersFetching());
-        request("http://localhost:3001/filters")
-            .then(data => dispatch(filtersFetched(data)))
-            .catch(() => dispatch(filtersFetchingError()))
+        dispatch(fetchFilters(request));
 
         // eslint-disable-next-line
     }, []);
