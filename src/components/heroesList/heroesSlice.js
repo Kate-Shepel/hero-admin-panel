963b6becb3f76e24bc935183a955e04a0d15544e
Kate-Shepel <kateborichevskaya@gmail.com>
2024-07-12 18:44:58 +0200

    refactor: transfer filter logics to the heroes slice

--- src/components/heroesList/heroesSlice.js
+++ src/components/heroesList/heroesSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk, createEntityAdapter } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk, createEntityAdapter, createSelector } from '@reduxjs/toolkit';
 import {useHttp} from '../../hooks/http.hook'
 
 const heroesAdapter = createEntityAdapter();
@@ -42,7 +42,19 @@ const {actions, reducer} = heroesSlice;
 
 export default reducer;
 
-export const {selectAll} = heroesAdapter.getSelectors(state => state.heroes)
+const {selectAll} = heroesAdapter.getSelectors(state => state.heroes);
+
+export const filteredHeroesSelector = createSelector(
+  (state) => state.filters.activeFilter,
+  selectAll,
+  (filter, heroes) => {
+      if (filter === 'all') {
+          return heroes
+      } else {
+          return heroes.filter(item => item.element === filter)
+      }
+  }
+);
 
 export const {
   heroesFetching,
